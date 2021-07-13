f4ba8469068142033b0e147be776b58e40ee6014
王 添一 <wangtianyi@souche.com>
2021-07-13 10:54:16 +0800

    配置文件

--- Carrot-Common/src/main/java/com/carrot/sec/config/CSearchConfig.java
+++ Carrot-Common/src/main/java/com/carrot/sec/config/CSearchConfig.java
@@ -6,11 +6,47 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.util.Properties;
 
 @Getter
 public class CSearchConfig {
+
+    private static String dataPath ;
+
+    static{
+
+        String configFile = System.getProperty("user.dir") + File.separator + "config.cfg";
+        File file = new File(configFile);
+        if(!file.exists()){
+            throw new RuntimeException("not found config file ! please create config.cfg in your path ! path is  : " + configFile);
+        }
+
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
+            Properties properties = new Properties();
+            properties.load(br);
+
+            dataPath = properties.getProperty("data.path");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (br != null){
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
 
     private Path path;
 
@@ -29,9 +65,9 @@ public class CSearchConfig {
         if(StringUtils.isEmpty(className)){
             throw new RuntimeException("className can't null !");
         }
+
         CSearchConfig cSearchConfig = new CSearchConfig();
-        cSearchConfig.setPath("/Users/wty/Downloads/temp" + File.separator + className.toLowerCase());
-//        cSearchConfig.setPath("d://temp" + File.separator + obj.getClass().getSimpleName().toLowerCase());
+        cSearchConfig.setPath(CSearchConfig.dataPath + File.separator + className.toLowerCase());
         cSearchConfig.setAnalyzer(new StandardAnalyzer());
         return cSearchConfig;
     }
--- Carrot-Search-Server/src/main/java/com/carrot/sec/CarrotServerMain.java
+++ Carrot-Search-Server/src/main/java/com/carrot/sec/CarrotServerMain.java
@@ -1,14 +1,21 @@
 package com.carrot.sec;
 
+import com.carrot.sec.config.CSearchConfig;
 import com.carrot.sec.server.CarrotSearchServer;
 
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class CarrotServerMain {
 
     public static void main(String[] args) throws IOException {
-        new Thread(new CarrotSearchServer(9527)).start();
-        System.out.println("started !");
+
+//        CSearchConfig cSearchConfig = new CSearchConfig();
+//        Path path = cSearchConfig.getPath();
+
+        CSearchConfig student = CSearchConfig.getConfig("student");
+//        new Thread(new CarrotSearchServer(9527)).start();
+        System.out.println("started !" + " " + System.getProperty("user.dir"));
     }
 
 }

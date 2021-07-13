68f532f9104eeee07001b7779217807778e518a8
王 添一 <wangtianyi@souche.com>
2021-07-13 10:19:35 +0800

    jar 包

--- Carrot-Search-Server/src/main/java/com/carrot/sec/CarrotServerMain.java
+++ Carrot-Search-Server/src/main/java/com/carrot/sec/CarrotServerMain.java
@@ -8,6 +8,7 @@ public class CarrotServerMain {
 
     public static void main(String[] args) throws IOException {
         new Thread(new CarrotSearchServer(9527)).start();
+        System.out.println("started !");
     }
 
 }

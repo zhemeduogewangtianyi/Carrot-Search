d43f901a090a6452a4cb755e65a6c538107592c1
王 添一 <wangtianyi@souche.com>
2021-07-13 10:56:22 +0800

    配置文件

--- Carrot-Search-Server/src/main/java/com/carrot/sec/CarrotServerMain.java
+++ Carrot-Search-Server/src/main/java/com/carrot/sec/CarrotServerMain.java
@@ -1,20 +1,13 @@
 package com.carrot.sec;
 
-import com.carrot.sec.config.CSearchConfig;
 import com.carrot.sec.server.CarrotSearchServer;
 
 import java.io.IOException;
-import java.nio.file.Path;
 
 public class CarrotServerMain {
 
     public static void main(String[] args) throws IOException {
-
-//        CSearchConfig cSearchConfig = new CSearchConfig();
-//        Path path = cSearchConfig.getPath();
-
-        CSearchConfig student = CSearchConfig.getConfig("student");
-//        new Thread(new CarrotSearchServer(9527)).start();
+        new Thread(new CarrotSearchServer(9527)).start();
         System.out.println("started !" + " " + System.getProperty("user.dir"));
     }
 
